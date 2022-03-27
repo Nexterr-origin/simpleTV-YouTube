2dcbf4786973f0d58366b7a1a5e42f5cd9a7bd3f
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-03-28 00:43:14 +0300

    GetVideoInfo: some fix age-gate
    only tier1
    https://github.com/yt-dlp/yt-dlp/pull/575#issuecomment-888837000

--- YT.lua
+++ YT.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для сайта https://www.youtube.com (12/2/22)
+-- видеоскрипт для сайта https://www.youtube.com (28/3/22)
 -- https://github.com/Nexterr-origin/simpleTV-YouTube
 --[[
 	Copyright © 2017-2022 Nexterr
@@ -2137,16 +2137,16 @@ local infoInFile = false
 		end
 	 return t
 	end
-	local function GetVideoInfo(clientScreen)
+	local function GetVideoInfo(clientName)
 		local session_videoInfo = m_simpleTV.Http.New(userAgent, proxy, false)
 			if not session_videoInfo then return end
 		m_simpleTV.Http.SetTimeout(session_videoInfo, 8000)
-		clientScreen = clientScreen or 'WATCH'
+		clientName = clientName or 'WEB'
 		local signTs = m_simpleTV.User.YT.signTs or 0
 		local visitorData = m_simpleTV.User.YT.visitorData or ''
 		local thirdParty = urlAdr:match('$OPT:http%-referrer=([^%$]+)') or 'https://www.youtube.com'
 		local headers = GetHeader_Auth() .. 'Content-Type: application/json\nX-Goog-Visitor-Id: ' .. visitorData
-		local body = string.format('{"videoId":"%s","context":{"client":{"hl":"%s","gl":"%s","clientName":"WEB","clientVersion":"1.20220208.09.00","clientScreen":"%s"},"thirdParty":{"embedUrl":"%s"}},"playbackContext":{"contentPlaybackContext":{"signatureTimestamp":%s}},"racyCheckOk":true,"contentCheckOk":true}', m_simpleTV.User.YT.vId, m_simpleTV.User.YT.Lng.lang, m_simpleTV.User.YT.Lng.country, clientScreen, thirdParty, signTs)
+		local body = string.format('{"videoId":"%s","context":{"client":{"hl":"%s","gl":"%s","clientName":"%s","clientVersion":"1.20220208.09.00"},"thirdParty":{"embedUrl":"%s"}},"playbackContext":{"contentPlaybackContext":{"signatureTimestamp":%s}},"racyCheckOk":true,"contentCheckOk":true}', m_simpleTV.User.YT.vId, m_simpleTV.User.YT.Lng.lang, m_simpleTV.User.YT.Lng.country, clientName, thirdParty, signTs)
 		local url = 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
 		m_simpleTV.Http.SetCookies(session_videoInfo, url, m_simpleTV.User.YT.cookies, '')
 		local rc, answer = m_simpleTV.Http.Request(session_videoInfo, {url = url, method = 'post', body = body, headers = headers})
@@ -2192,7 +2192,7 @@ local infoInFile = false
 		if not m_simpleTV.User.YT.isAuth
 			and player_response:match('status":%s*"LOGIN')
 		then
-			local rc_LR, player_response_LR = GetVideoInfo('EMBED')
+			local rc_LR, player_response_LR = GetVideoInfo('WEB_EMBEDDED_PLAYER')
 			if player_response_LR
 				and player_response_LR:match('status":%s*"OK')
 			then
