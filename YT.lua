ffdef36a3c3129e84a41967b8a258a7017cb5d95
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2022-08-03 08:53:17 +0300

    GetVideoInfo: change clientVersion

--- YT.lua
+++ YT.lua
@@ -2142,7 +2142,7 @@ local infoInFile = false
 			if not session_videoInfo then return end
 		m_simpleTV.Http.SetTimeout(session_videoInfo, 8000)
 		clientName = clientName or 'WEB'
-		clientVersion = clientVersion or '2.20220728.00.00'
+		clientVersion = clientVersion or '1.20220728.00.00'
 		local signTs = m_simpleTV.User.YT.signTs or 0
 		local visitorData = m_simpleTV.User.YT.visitorData or ''
 		local thirdParty = urlAdr:match('$OPT:http%-referrer=([^%$]+)') or 'https://www.youtube.com/'
